84e09224316850b24d8ca3306c3e7f938ac29bd9
ccchippp <clempke90@gmail.com>
2021-03-29 12:16:04 -0400

    map loads

--- src/GoogleApiWrapper.js
+++ src/GoogleApiWrapper.js
@@ -15,8 +15,8 @@ class MapContainer extends Component {
         style={mapStyles}
         initialCenter={
           {
-            lat: -1.2884,
-            lng: 36.8233
+            lat: 38.9072,
+            lng: -77.0369
           }
         }
       />
