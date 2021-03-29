f0f3c70e7a2d60d9fa9b96c624c48a406966b488
John Fuquene <jfuquene@email.wm.edu>
2021-03-29 13:53:40 -0400

    inital bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
+import React from "react";
 import './App.css';
+import bootstrap from 'bootstrap';
+import Button from './Button';
+import { Alert, Card, Form, Container, Row, Col  } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Container> 
+        <Form>
+          
+          <Form.Group>
+            <Form.Label> Enter your city: </Form.Label>
+            <Form.Control placeholder="name of a city"/>
+            <Form.Text className="text-muted"> 
+            Nice spot!
+            </Form.Text>
+          </Form.Group>
+
+        </Form>
+        <Card className="mb-3" style={{ color: "#000"}}>
+          <Card.Img src="https://www.fws.gov/refuges/friends/friendsLocatorMaps/images/USmap.gif" />
+          <Card.Body>
+            <Card.Title> Title</Card.Title>
+              <Card.Text> This is an example</Card.Text>
+              <Button variant="primary">Read More</Button>
+           </Card.Body>   
+        </Card>  
+        
+        <Alert variant="success">This is a button</Alert>
+        <Button /> 
+        </Container>
       </header>
-      <div>
-      </div>
+  
     </div>
 
   );
---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import bootstrap from 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+export default class Button extends Component {
+render() {
+    return (
+        <div>
+            <button type="button" className="btn btn-primary">
+            Click me!
+            </button>
+        </div>
+    )
+
+}
+
+}
