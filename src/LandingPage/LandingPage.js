62c2290147012eacdcdb31fd6c0b82fe586e2504
ccchippp <clempke90@gmail.com>
2021-03-30 14:53:11 -0400

    added Bulma

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { SearchBar } from '../SearchBar/SearchBar'
 import { TopNav } from '../TopNav/TopNav'
+import styles from './LandingPage.module.css'
 
 export function LandingPage() {
     return (
         <div>
             <TopNav/>
+            <SearchBar/>
         </div>
     )
 }
---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './SearchBar.module.css'
+
+export function SearchBar() {
+    return (
+        <div>
+        </div>
+    )
+}
