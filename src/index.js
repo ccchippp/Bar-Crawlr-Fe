dcc0f52b7b01c535a24350bfa90f35134983d89d
ccchippp <clempke90@gmail.com>
2021-03-30 16:11:02 -0400

    added react router

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bulma/css/bulma.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
