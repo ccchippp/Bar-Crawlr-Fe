424254bec03c1b664ce600aaecfdcdb6354b1e35
ccchippp <clempke90@gmail.com>
2021-03-30 15:46:11 -0400

    search button

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -3,7 +3,34 @@ import styles from './SearchBar.module.css'
 
 export function SearchBar() {
     return (
-        <div>
+        <div className="field has-addons">
+       <p className='control'>
+            <button className='button is-static is-medium'>Search</button>
+       </p>
+       <p className='control'>
+            <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="Start Crawling" />
+       </p>
+       <p className='control'>
+            <button className='button is-static is-medium'>NEAR</button>
+       </p>
+       <p className='control'>
+            <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="Where are you" />
+       </p>
+       <div className={`button is-medium ${styles['search-button']}`}>
+           <span className='icon'>
+            <i className="fas fa-search"></i>
+           </span>
+       </div>
         </div>
+        // <div class="field has-addons">
+        //     <div class="control">
+        //         <input class="input" type="text" placeholder="Find a repository" />
+        // </div>
+        // <div class="control">
+        //     <a class="button is-info">
+        //         Start your Crawl
+        //      </a>
+        // </div>
+        // </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import 'bulma/css/bulma.css'
 
 ReactDOM.render(
     <App />,
